e5aabee10c1c3b31044bcf37df6c702fcda5bbf3
doyou1 <tla149@daum.net>
2024-03-03 22:33:17 +0900

    - axios interceptor, request시 저장된 token도 함께

--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -1,10 +1,46 @@
 import axios from "axios";
+import { useAccessTokenStore } from "@/store";
+const TOKEN_REISSUE_URL = "/api/user/reissue";
 
 export const useAxios = () => {
   const axiosConfig: { baseURL: string; timeout: number } = {
     baseURL: import.meta.env.VITE_API_BASE_URL,
     timeout: 30000, // 30s
   };
+  const api = axios.create(axiosConfig);
+  const { getAccessToken } = useAccessTokenStore();
+  api.interceptors.request.use(
+    (config) => {
+      if (!config.headers) return config;
+      const token = getAccessToken();
+      if (token !== undefined) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
 
-  return axios.create(axiosConfig);
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    },
+  );
+
+  api.interceptors.response.use(
+    (config) => {
+      if (!config.headers) return config;
+      return config;
+    },
+    (error) => {
+      /** reissue url의 response가 아니면서 403 forbidden인 경우,
+       * reissue 요청을 보내야함 reissue에서까지 403 등 에러라면, 로그인해야함 */
+      if (
+        error.response.path !== TOKEN_REISSUE_URL &&
+        error.response.status === 403
+      ) {
+        console.log("need to call reissue");
+      }
+      return Promise.reject(error);
+    },
+  );
+
+  return api;
 };
