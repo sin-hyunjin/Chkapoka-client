2e2ff7708712134d2ebe509144bc5e269f1ad2c4
doyou1 <tla149@daum.net>
2024-03-27 19:59:50 +0900

    - vue-query 타입 추론

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,14 @@
 module.exports = {
+  root: true,
   env: {
     node: true,
-    browser: true,
-    es2023: true,
   },
   extends: [
-    "eslint:recommended",
     "plugin:vue/vue3-recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
+    "eslint:recommended",
+    "@vue/typescript/recommended",
+    "@vue/prettier",
+    "@vue/prettier/@typescript-eslint",
     "plugin:storybook/recommended",
   ],
   overrides: [
@@ -22,10 +22,8 @@ module.exports = {
       },
     },
   ],
-  parser: "vue-eslint-parser",
   parserOptions: {
-    parser: "@typescript-eslint/parser",
-    sourceType: "module",
+    ecmaVersion: 2020,
   },
   plugins: ["@typescript-eslint", "vue"],
   rules: {
