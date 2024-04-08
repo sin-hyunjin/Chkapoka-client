39de3f8c9d62632d23dd0ffd83de6afa87fa12f0
doyou1 <tla149@daum.net>
2024-04-09 08:25:08 +0900

    - 트리 완성하기 다이얼로그     - ? 아이콘으로 툴팁 표시 "트리 소유자 유형", "트리 아이템 열람" 유형에 대한 설명 추가

--- src/components/tree/create/SelectOptionsDialog.vue
+++ src/components/tree/create/SelectOptionsDialog.vue
@@ -14,13 +14,44 @@
         <div class="header cp-text-head-4">트리 완성하기</div>
         <div class="body">
           <div class="select-type">
-            <div class="title cp-text-title-2">트리 소유자 유형</div>
+            <div class="title cp-text-title-2">
+              트리 소유자 유형
+              <el-tooltip placement="top">
+                <template #content>
+                  <div class="tooltip-content">
+                    <div>내트리는 트리의 소유권, 주인이 작성자인 경우</div>
+                    <div>
+                      미부여 트리는 트리의 소유권, 주인이 작성자가 아닌 다른
+                      유저로서 추후에 주인에게 전달, 공유할 예정일 경우
+                    </div>
+                  </div>
+                </template>
+                <icon-help class="icon" />
+              </el-tooltip>
+            </div>
             <cp-radio-group
               v-model="innerOptions.ownerType"
               type="button"
               :items="ownerTypeItems"
             />
-            <div class="title cp-text-title-2">트리 아이템 열람 유형</div>
+            <div class="title cp-text-title-2">
+              트리 아이템 열람 유형
+              <el-tooltip placement="top">
+                <template #content>
+                  <div class="tooltip-content">
+                    <div>
+                      트리 아이템 열람 유형은 작성된 트리 아이템의 열람 권한에
+                      대한 설정이다.
+                    </div>
+                    <div>
+                      혼자보기는 트리 주인과 트리 아이템 작성자, 다같이보기는
+                      누구나 해당 트리 아이템을 열람하는 것이 가능하다.
+                    </div>
+                  </div>
+                </template>
+                <icon-help class="icon" />
+              </el-tooltip>
+            </div>
             <cp-radio-group
               v-model="innerOptions.shareType"
               type="button"
@@ -63,7 +94,9 @@ export default defineComponent({
 import CpDialog from "@/components/commons/CpDialog.vue";
 import CpButton from "@/components/commons/CpButton.vue";
 import CpRadioGroup from "@/components/commons/CpRadioGroup.vue";
+import IconHelp from "@/components/commons/images/IconHelp.vue";
 import { OwnerType, ShareType } from "@/composables/use-tree-create-controller";
+import { ElTooltip } from "element-plus";
 
 const props = defineProps<{
   visible: boolean;
@@ -103,8 +136,6 @@ const shareTypeItems = [
 <style scoped lang="scss">
 .container {
   margin: 0 auto;
-
-  width: 80%;
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -119,15 +150,25 @@ const shareTypeItems = [
   .body {
     display: flex;
     flex-direction: column;
-    width: 100%;
-    align-items: center;
     margin: var(--cp-number-24) 0;
     .select-type {
+      display: flex;
+      flex-direction: column;
       color: var(--cp-color-black);
 
       .title {
         margin: var(--cp-number-8) 0;
+        display: flex;
+        align-items: center;
+
+        .icon {
+          margin-left: var(--cp-number-4);
+        }
       }
+    }
+
+    .tooltip-content {
+      max-width: 200px;
     }
   }
 
